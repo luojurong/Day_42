adf6f25d78e885b6fd5772b34258d3fbd85c8c3a
dengnan <476831724@qq.com>
2017-03-02 19:19:23 +0800

    2017年3月2日19:19:15

--- app/src/main/java/com/beautiful/beautiful/activity/MainActivity.java
+++ app/src/main/java/com/beautiful/beautiful/activity/MainActivity.java
@@ -1,9 +1,13 @@
 package com.beautiful.beautiful.activity;
 
+import android.support.annotation.IdRes;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.beautiful.beautiful.R;
 import com.beautiful.beautiful.fragment.FragmentHome;
@@ -12,29 +16,76 @@ import com.beautiful.beautiful.fragment.FragmentNine;
 import com.beautiful.beautiful.fragment.FragmentShop;
 import com.beautiful.beautiful.fragment.FragmentSift;
 
-import org.xutils.view.annotation.ViewInject;
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.x;
 
+@ContentView(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
     private FragmentManager manager;
-    private FragmentTransaction fragmentTransaction;
+    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         initData();
 
+        x.view().inject(this);
 
     }
+
+    private FragmentHome fragmentHome = new FragmentHome();
+    private FragmentMy fragmentMy = new FragmentMy();
+    private FragmentNine fragmentNine = new FragmentNine();
+    private FragmentShop fragmentShop = new FragmentShop();
+    private FragmentSift fragmentSift = new FragmentSift();
 
     private void initData() {
         manager = getSupportFragmentManager();
-        fragmentTransaction = manager.beginTransaction();
-        fragmentTransaction.add(R.id.fragmentLayout_home,new FragmentHome());
-        fragmentTransaction.add(R.id.fragmentLayout_home,new FragmentMy());
-        fragmentTransaction.add(R.id.fragmentLayout_home,new FragmentNine());
-        fragmentTransaction.add(R.id.fragmentLayout_home,new FragmentShop());
-        fragmentTransaction.add(R.id.fragmentLayout_home,new FragmentSift());
+        transaction = manager.beginTransaction();
+//        transaction.add(R.id.fragmentLayout_home, fragmentHome)
+//                .add(R.id.fragmentLayout_home, fragmentMy)
+//                .add(R.id.fragmentLayout_home, fragmentNine)
+//                .add(R.id.fragmentLayout_home, fragmentShop)
+//                .add(R.id.fragmentLayout_home, fragmentSift);
+
+        transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragmentLayout_home, fragmentHome).commit();
+
     }
+
+
+    /**
+     * radiobutton
+     */
+    @Event(type = RadioGroup.OnCheckedChangeListener.class, value = R.id.rg)
+    private void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+            Log.e("---",checkedId+"---------");
+        switch (checkedId) {
+            case R.id.btn1:
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragmentLayout_home, fragmentHome).commit();
+                break;
+            case R.id.btn2:
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragmentLayout_home, fragmentNine).commit();
+                break;
+            case R.id.btn3:
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragmentLayout_home, fragmentSift).commit();
+                break;
+            case R.id.btn4:
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragmentLayout_home, fragmentShop).commit();
+                break;
+            case R.id.btn5:
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragmentLayout_home, fragmentMy).commit();
+                break;
+
+
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/beautiful/beautiful/presenter/MainActivityFrameLayout.java
@@ -0,0 +1,9 @@
+package com.beautiful.beautiful.presenter;
+
+/**
+ * Created by dengn on 2017/3/2.
+ */
+
+public class MainActivityFrameLayout {
+
+}
