a1061380c57374c3a2839fbaf0d5af0883b018ab
dengnan <476831724@qq.com>
2017-03-02 16:35:54 +0800

    2017年3月2日16:34:58

--- app/src/main/java/com/beautiful/beautiful/activity/MainActivity.java
+++ app/src/main/java/com/beautiful/beautiful/activity/MainActivity.java
@@ -1,15 +1,40 @@
 package com.beautiful.beautiful.activity;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.beautiful.beautiful.R;
+import com.beautiful.beautiful.fragment.FragmentHome;
+import com.beautiful.beautiful.fragment.FragmentMy;
+import com.beautiful.beautiful.fragment.FragmentNine;
+import com.beautiful.beautiful.fragment.FragmentShop;
+import com.beautiful.beautiful.fragment.FragmentSift;
+
+import org.xutils.view.annotation.ViewInject;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager manager;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+
+
+    }
+
+    private void initData() {
+        manager = getSupportFragmentManager();
+        fragmentTransaction = manager.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentLayout_home,new FragmentHome());
+        fragmentTransaction.add(R.id.fragmentLayout_home,new FragmentMy());
+        fragmentTransaction.add(R.id.fragmentLayout_home,new FragmentNine());
+        fragmentTransaction.add(R.id.fragmentLayout_home,new FragmentShop());
+        fragmentTransaction.add(R.id.fragmentLayout_home,new FragmentSift());
     }
 }
